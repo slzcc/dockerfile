430f28203a99ff41191f351c400bc1e164e82cc0
Jones Shi <383821949@qq.com>
2023-03-01 18:24:54 +0800

    add top-metrics

--- top-metrics/top.sh
+++ top-metrics/top.sh
@@ -2,6 +2,7 @@
 
 script_dir=/data/script
 workdir=${workdir:-/data/logs/top}
+IFTOP_WaitingTime=${IFTOP_WaitingTime:-100}
 dt=`date +"%Y%m%d%H%M"`
  
 mkdir -p ${workdir} && chmod 777 ${workdir}
@@ -15,7 +16,7 @@ top -b -n2 -d5 -c >> ${workdir}/top.out$dt
 iotop -t -o --iter=1 >> ${workdir}/iotop.out$dt
  
 # [ `rpm -aq | grep -c iftop` -eq 0 ] && yum install -y iftop
-iftop -i $(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[t]*//g' | sed 's/[:]*$//g'|grep -E "^(ens|eth)") -N -P -t -L 50 -o 40s -s 100 > ${workdir}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
+iftop -i $(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[t]*//g' | sed 's/[:]*$//g'|grep -E "^(ens|eth)") -N -P -t -L 50 -o 40s -s ${IFTOP_WaitingTime} > ${workdir}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
 #iftop -i eth0 -N -P -t -L 50 -o 40s -s 100 > ${workdir}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
  
 ps -auxH -wwwwwwwww >> ${workdir}/psaux.out$dt

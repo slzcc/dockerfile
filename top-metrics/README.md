9c1c80966d2452d2d64031bf57a2ee418ca791c8
Jones Shi <383821949@qq.com>
2023-03-03 10:28:17 +0800

    add top_metrics

--- top-metrics/README.md
+++ top-metrics/README.md
@@ -16,3 +16,5 @@ docker run --pull=always --pid=host --net=host --privileged=true --rm -t -v /dat
 其中可以添加环境变量 `IFTOP_WaitingTime` 控制获取流量时长, 默认 100 秒.
 
 默认 `gz` 文件保留 3 天, 如需修改请传递环境变量 `HISTORY_RESERVE` 解决.
+
+日志默认存放 `/data/logs/top` 中, 使用变量 `LOG_DIR` 环境变量控制.
--- top-metrics/top.sh
+++ top-metrics/top.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-script_dir=/data/script
-workdir=${workdir:-/data/logs/top}
+LOG_DIR=${LOG_DIR:-/data/logs/top}
 IFTOP_WaitingTime=${IFTOP_WaitingTime:-100}
 HISTORY_RESERVE=${HISTORY_RESERVE:-3}
 dt=`date +"%Y%m%d%H%M"`
  
-mkdir -p ${workdir} && chmod 777 ${workdir}
+# mkdir -p ${LOG_DIR} && chmod 777 ${LOG_DIR}
  
 # [ `rpm -aq | grep -c epel-release` -eq 0 ] && yum install -y epel-release
  
-python3 ${script_dir}/ps_mem.py >> ${workdir}/ps_mem.out$dt
+python3 /usr/local/bin/ps_mem.py >> ${LOG_DIR}/ps_mem.out$dt
  
-top -b -n2 -d5 -c >> ${workdir}/top.out$dt
+top -b -n2 -d5 -c >> ${LOG_DIR}/top.out$dt
 # [ `rpm -aq | grep -c iotop` -eq 0 ] && yum install -y iotop
-iotop -t -o --iter=1 >> ${workdir}/iotop.out$dt
+iotop -t -o --iter=1 >> ${LOG_DIR}/iotop.out$dt
  
 # [ `rpm -aq | grep -c iftop` -eq 0 ] && yum install -y iftop
-iftop -i $(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[t]*//g' | sed 's/[:]*$//g'|grep -E "^(ens|eth)") -N -P -t -L 50 -o 40s -s ${IFTOP_WaitingTime} > ${workdir}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
-#iftop -i eth0 -N -P -t -L 50 -o 40s -s 100 > ${workdir}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
+iftop -i $(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[t]*//g' | sed 's/[:]*$//g'|grep -E "^(ens|eth)") -N -P -t -L 50 -o 40s -s ${IFTOP_WaitingTime} > ${LOG_DIR}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
+#iftop -i eth0 -N -P -t -L 50 -o 40s -s 100 > ${LOG_DIR}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
  
-ps -auxH -wwwwwwwww >> ${workdir}/psaux.out$dt
+ps -auxH -wwwwwwwww >> ${LOG_DIR}/psaux.out$dt
  
-gzip ${workdir}/*$dt
+gzip ${LOG_DIR}/*$dt
  
-find ${workdir} -mtime +${HISTORY_RESERVE} -type f -delete
+find ${LOG_DIR} -mtime +${HISTORY_RESERVE} -type f -delete

2558632cf13ad2e91f104151895f78c837185209
Jones Shi <383821949@qq.com>
2023-03-03 11:34:23 +0800

    add top_metrics

--- top-metrics/top.sh
+++ top-metrics/top.sh
@@ -13,11 +13,16 @@ dt=`date +"%Y%m%d%H%M"`
  
 mkdir -p ${LOG_DIR} && chmod 777 ${LOG_DIR}
 
+# Install Iftop
+# [ `rpm -aq | grep -c iftop` -eq 0 ] && yum install -y iftop
+## Iotop Script
+iftop -i $(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[t]*//g' | sed 's/[:]*$//g'|grep -E "^(ens|eth)") -N -P -t -L 50 -o 40s -s ${IFTOP_WaitingTime} >> ${LOG_DIR}/iftop.out`date +"%Y%m%d%H%M"` &
+
 # Install Epel-Release 
-[ `rpm -aq | grep -c epel-release` -eq 0 ] && yum install -y epel-release
+# [ `rpm -aq | grep -c epel-release` -eq 0 ] && yum install -y epel-release
 
 # Install dstat
-[ `rpm -aq | grep -c dstat` -eq 0 ] && yum install -y dstat
+# [ `rpm -aq | grep -c dstat` -eq 0 ] && yum install -y dstat
 ## Dstat Script
 dstat 1 ${DSTAT_WaitingTime} >> ${LOG_DIR}/dstat.out$dt &
 
@@ -33,20 +38,15 @@ done
 ## Top Script
 top -b -n2 -d5 -c >> ${LOG_DIR}/top.out$dt &
 
+## PS Script
+ps -auxH -wwwwwwwww >> ${LOG_DIR}/psaux.out$dt &
+
 # Install Iotop
-[ `rpm -aq | grep -c iotop` -eq 0 ] && yum install -y iotop
+# [ `rpm -aq | grep -c iotop` -eq 0 ] && yum install -y iotop
 ## Iotop Script
 for item in $(seq 1 ${IOTOP_WaitingTime});do
-    iotop -t -o --iter=1 >> ${LOG_DIR}/iotop.out$dt &
+    iotop -t -o --iter=1 >> ${LOG_DIR}/iotop.out$dt
 done
-
-# Install Iftop
-[ `rpm -aq | grep -c iftop` -eq 0 ] && yum install -y iftop
-## Iotop Script
-iftop -i $(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[t]*//g' | sed 's/[:]*$//g'|grep -E "^(ens|eth)") -N -P -t -L 50 -o 40s -s ${IFTOP_WaitingTime} > ${LOG_DIR}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
-
-## PS Script
-ps -auxH -wwwwwwwww >> ${LOG_DIR}/psaux.out$dt
 
 # Compressed Files
 gzip ${LOG_DIR}/*$dt

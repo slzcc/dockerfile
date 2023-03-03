ce11a3323504b425aac90eef969f99c9de0097e6
Jones Shi <383821949@qq.com>
2023-03-03 11:03:35 +0800

    add top_metrics

--- top-metrics/README.md
+++ top-metrics/README.md
@@ -13,7 +13,10 @@ USE
 docker run --pull=always --pid=host --net=host --privileged=true --rm -t -v /data/logs/top:/data/logs/top slzcc/top-metrics:0.0.1
 ```
 
-其中可以添加环境变量 `IFTOP_WaitingTime` 控制获取流量时长, 默认 100 秒.
+其中可以添加环境变量 `IFTOP_WaitingTime` 控制 `iftop` 获取流量时长, 默认 100 秒.
+
+其中可以添加环境变量 `DSTAT_WaitingTime` 控制 `dstat` 获取指标时长, 默认 100 秒.
+
 
 默认 `gz` 文件保留 3 天, 如需修改请传递环境变量 `HISTORY_RESERVE` 解决.
 
--- top-metrics/top.sh
+++ top-metrics/top.sh
@@ -2,25 +2,47 @@
 
 LOG_DIR=${LOG_DIR:-/data/logs/top}
 IFTOP_WaitingTime=${IFTOP_WaitingTime:-100}
+DSTAT_WaitingTime=${DSTAT_WaitingTime:-100}
 HISTORY_RESERVE=${HISTORY_RESERVE:-3}
 dt=`date +"%Y%m%d%H%M"`
  
-# mkdir -p ${LOG_DIR} && chmod 777 ${LOG_DIR}
- 
-# [ `rpm -aq | grep -c epel-release` -eq 0 ] && yum install -y epel-release
- 
+mkdir -p ${LOG_DIR} && chmod 777 ${LOG_DIR}
+
+# Install Epel-Release 
+[ `rpm -aq | grep -c epel-release` -eq 0 ] && yum install -y epel-release
+
+# Install dstat
+[ `rpm -aq | grep -c dstat` -eq 0 ] && yum install -y dstat
+## Dstat Script
+dstat 1 ${DSTAT_WaitingTime} >> {LOG_DIR}/dstat.out$dt &
+
+## Ps_mem Script
+## https://github.com/pixelb/ps_mem/blob/master/ps_mem.py
 python3 /usr/local/bin/ps_mem.py >> ${LOG_DIR}/ps_mem.out$dt
- 
+for item in $(ps aux|awk '{print $2}');do
+   echo ">>>>>>>>>>>>>>>>>>>>>> PID $item" >> ${LOG_DIR}/ps_mem.out$dt
+   ps_mem.py -p $item >> ${LOG_DIR}/ps_mem.out$dt
+   echo "" >> ${LOG_DIR}/ps_mem.out$dt
+done
+
+## Top Script
 top -b -n2 -d5 -c >> ${LOG_DIR}/top.out$dt
-# [ `rpm -aq | grep -c iotop` -eq 0 ] && yum install -y iotop
+
+# Install Iotop
+[ `rpm -aq | grep -c iotop` -eq 0 ] && yum install -y iotop
+## Iotop Script
 iotop -t -o --iter=1 >> ${LOG_DIR}/iotop.out$dt
  
-# [ `rpm -aq | grep -c iftop` -eq 0 ] && yum install -y iftop
+# Install Iftop
+[ `rpm -aq | grep -c iftop` -eq 0 ] && yum install -y iftop
+## Iotop Script
 iftop -i $(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[t]*//g' | sed 's/[:]*$//g'|grep -E "^(ens|eth)") -N -P -t -L 50 -o 40s -s ${IFTOP_WaitingTime} > ${LOG_DIR}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
-#iftop -i eth0 -N -P -t -L 50 -o 40s -s 100 > ${LOG_DIR}/iftop.out`date +"%Y%m%d%H%M"` 2>&1
- 
+
+## PS Script
 ps -auxH -wwwwwwwww >> ${LOG_DIR}/psaux.out$dt
- 
+
+# Compressed Files
 gzip ${LOG_DIR}/*$dt
- 
+
+# Delete Files Periodically
 find ${LOG_DIR} -mtime +${HISTORY_RESERVE} -type f -delete
